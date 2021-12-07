e8d866e246663b69b50fbf7a2ccfbcaed81ec78e
cnhjp <1304272317@qq.com>
2021-12-07 23:52:32 +0800

    feat(demo): button demo

--- src/views/demos/button/index.vue
+++ src/views/demos/button/index.vue
@@ -1,1 +1,7 @@
-<template>button</template>
+<template>
+    <h-button>default</h-button>
+    <h-button type="primary">primary</h-button>
+    <h-button type="danger">danger</h-button>
+    <h-button size="small">small</h-button>
+    <h-button size="medium">medium</h-button>
+</template>
