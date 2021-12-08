155668cd4905c2bf9d5b70693687d29845e6ebd7
hjp <jp.huang@hongzhengtech.com>
2021-12-08 15:24:09 +0800

    refactor(test): change test folder position

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -10,4 +10,5 @@
     <div class="blue">blue</div>
     <div class="h-red">red</div>
     <h-button circle type="primary">hello</h-button>
+    <h-button round size="small">this is my house</h-button>
 </template>
